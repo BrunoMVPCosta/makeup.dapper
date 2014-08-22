d4f0632813d08ed46640894c319ee20048d617c4
BrunoMVPCosta <kele_bru@hotmail.com>
2014-08-22 08:40:45 +0100

    Refactoring integration tests

---
+++ integration/Integration.Test/TestDbContext.cs
@@ -0,0 +1,30 @@
+﻿using MakeUp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Integration.Test
+{
+    public class TestDbContext : DapperContext
+    {
+        public IQueryable<User> Users
+        {
+            get
+            {
+                return Query<User>();
+            }
+        }
+
+        public TestDbContext(string connection)
+            : base(connection)
+        {
+            this.DatabaseModel.AddMapping(new UserEntityMapping());
+        }
+
+        public TestDbContext()
+        {
+            this.DatabaseModel.AddMapping(new UserEntityMapping());
+        }
+    }
+}
--- integration/Integration.Test/UnitTest1.cs
+++ integration/Integration.Test/UnitTest1.cs
@@ -19,30 +19,30 @@ namespace Integration.Test
         public void Initialize()
         {
             using (var connection = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=tempdb;Integrated Security=True"))
-                        {
-                            connection.Open();
-                            try
-                            {
-                                connection.Execute(@"ALTER DATABASE DapperSimpleCrudTestDb SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+            {
+                connection.Open();
+                try
+                {
+                    connection.Execute(@"ALTER DATABASE DapperSimpleCrudTestDb SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
                                                      DROP DATABASE DapperSimpleCrudTestDb ; ");
-                            }
-                            catch { }
+                }
+                catch { }
 
-                            connection.Execute(@" CREATE DATABASE DapperSimpleCrudTestDb; ");
-                        }
+                connection.Execute(@" CREATE DATABASE DapperSimpleCrudTestDb; ");
+            }
 
-                        using (IDbConnection connection = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=DapperSimpleCrudTestDb;Integrated Security=True"))
-                        {
-                            connection.Open();
-                            connection.Execute(@" create table Users (Id int IDENTITY(1,1) not null, Name nvarchar(100) not null, Age int not null, ScheduledDayOff int null) ");
-                            connection.Execute(@" create table Car (CarId int IDENTITY(1,1) not null, Make nvarchar(100) not null, Model nvarchar(100) not null) ");
-                            connection.Execute(@" create table City (Name nvarchar(100) not null, Population int not null, Version rowversion) ");
-                            connection.Execute(@" CREATE SCHEMA Log; ");
-                            connection.Execute(@" create table Log.CarLog (Id int IDENTITY(1,1) not null, LogNotes nvarchar(100) NOT NULL) ");
+            using (IDbConnection connection = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=DapperSimpleCrudTestDb;Integrated Security=True"))
+            {
+                connection.Open();
+                connection.Execute(@" create table Users (Id int IDENTITY(1,1) not null, Name nvarchar(100) not null, Age int not null, ScheduledDayOff int null) ");
+                connection.Execute(@" create table Car (CarId int IDENTITY(1,1) not null, Make nvarchar(100) not null, Model nvarchar(100) not null) ");
+                connection.Execute(@" create table City (Name nvarchar(100) not null, Population int not null, Version rowversion) ");
+                connection.Execute(@" CREATE SCHEMA Log; ");
+                connection.Execute(@" create table Log.CarLog (Id int IDENTITY(1,1) not null, LogNotes nvarchar(100) NOT NULL) ");
 
-                            connection.Execute("INSERT INTO USERS VALUES ('teste', 21, 1)");
-                        }
-                        Console.WriteLine("Created database");
+                connection.Execute("INSERT INTO USERS VALUES ('teste', 21, 1)");
+            }
+            Console.WriteLine("Created database");
         }
 
         [TestMethod]
@@ -59,49 +59,6 @@ namespace Integration.Test
                     Debug.WriteLine(user.Name);
                 }
             }
-        }
-    }
-
-    public class User
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public DayOfWeek? ScheduledDayOff { get; set; }
-    }
-
-    public class UserEntityMapping : EntityMapper<User>
-    {
-        public UserEntityMapping()
-        {
-            this.ToTable("Users");
-
-            this.Property(p => p.Id).ToColumn("Id");
-            this.Property(p => p.Name).ToColumn("Name");
-            this.Property(p => p.Age).ToColumn("Age");
-            this.Property(p => p.ScheduledDayOff).ToColumn("ScheduledDayOff").Ignore();
-        }
-    }
-
-    public class TestDbContext : DapperContext
-    {
-        public IQueryable<User> Users
-        {
-            get
-            {
-                return Query<User>();
-            }
-        }
-
-        public TestDbContext(string connection)
-            : base(connection)
-        {
-            this.DatabaseModel.AddMapping(new UserEntityMapping());
-        }
-
-        public TestDbContext()
-        {
-            this.DatabaseModel.AddMapping(new UserEntityMapping());
         }
     }
 }
